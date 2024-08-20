e042f48aab2ed88920ff3323b362178b38045859
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-08-19 19:34:12 -0600

    Agregando estado para guardar formulario / adding stato to save form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,36 @@
+import { useState, ChangeEvent } from "react";
 import { categories } from "../data/Categories";
 
 const Form = () => {
+  const [activity, setActivity] = useState({
+    category: "",
+    name: "",
+    calories: 0,
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    // console.log(e.target.id);
+    // console.log(e.target.value);
+
+    setActivity({
+      ...activity,
+      [e.target.id]: e.target.value,
+    });
+  };
+
   return (
     <form className="space-y-5 bg-white p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="categories" className="font-bold">
+        <label htmlFor="category" className="font-bold">
           Categoria:
         </label>
         <select
           className="border-slate-300 p-2 rounded-lg w-full"
-          id="categories"
+          id="category"
+          value={activity.category}
+          onChange={handleChange}
         >
           {categories.map((item) => (
             <option key={item.id} value={item.id}>
@@ -20,14 +41,16 @@ const Form = () => {
       </div>
 
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">
+        <label htmlFor="name" className="font-bold">
           Actividad
         </label>
         <input
-          id="activity"
+          id="name"
           type="text"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de naranja"
+          value={activity.name}
+          onChange={handleChange}
         />
       </div>
 
@@ -40,6 +63,8 @@ const Form = () => {
           type="number"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Calorias. ej 500"
+          value={activity.calories}
+          onChange={handleChange}
         />
       </div>
 
